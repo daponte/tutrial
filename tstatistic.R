6cd97164ec9acdf16113f8cf43bb9a73e6564a24
Lorenzo Daponte <e.vuelta@gmail.com>
2014-08-03 23:24:00 +0900

    Initial Commit

---
+++ tstatistic.R
@@ -0,0 +1,8 @@
+tstatistic <- function(x,y){
+  m <- length(x)
+  n <- length(y)
+  sp <- sqrt(((m-1)*sd(x)^2+(n-1)*sd(y)^2)/(m+n-2))
+  t.stat <- (mean(x)-mean(y))/(sp*sqrt(1/m+1/n))
+  return (t.stat) ;;message 
+  
+}
