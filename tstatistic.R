8808b555a0e1ccfd22cbd6c46120bc6f56b73566
Lorenzo Daponte <e.vuelta@gmail.com>
2014-08-03 23:24:37 +0900

    comment out

--- tstatistic.R
+++ tstatistic.R
@@ -3,6 +3,5 @@ tstatistic <- function(x,y){
   n <- length(y)
   sp <- sqrt(((m-1)*sd(x)^2+(n-1)*sd(y)^2)/(m+n-2))
   t.stat <- (mean(x)-mean(y))/(sp*sqrt(1/m+1/n))
-  return (t.stat) ;;message 
-  
+  return (t.stat)
 }
